f4ca74a1dfe664fdecd16ddd5b716ca13ee8be77
JuanCM <juancampuzano2356@gmail.com>
2025-06-13 11:26:31 -0500

    feature: Add a fetchUsers service

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,1 +1,12 @@
-export const fetchUsers = () => {};
+import axios from "axios";
+
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+export const fetchUsers = async () => {
+    try {
+        const response = await axios.get(API_URL);
+        return response.data;
+    } catch (error) {
+        throw new Error("Error al cargar los usuarios");
+    }
+};
